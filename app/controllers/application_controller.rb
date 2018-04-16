b1491ac64b976c1c5d50b0133666643a44eb64ee
Kelsey Fecho <kfecho30@gmail.com>
2018-04-16 01:52:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/recipes' do
-    @recipe = Recipe.new()
+    @recipe = Recipe.new
     binding.pry
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
