8ec15af3d64bddd4660bf213c6c8e5c9dac4787f
Kelsey Fecho <kfecho30@gmail.com>
2018-04-16 01:56:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,7 @@ class ApplicationController < Sinatra::Base
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
+    @recipe.save
     redirect '/recipes/:id'
   end
 
